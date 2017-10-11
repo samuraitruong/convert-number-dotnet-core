a70a03a65525ac605361ac3deea7764ac1cf45de
samuraitruong <samuraitruong@hotmail.com>
2017-10-11 23:55:51 +1100

    finish core engine with TTD

--- AKAQ.CodeChallenge.Business/NumberConverterService.cs
+++ AKAQ.CodeChallenge.Business/NumberConverterService.cs
@@ -1,23 +1,66 @@
 ﻿using System;
 namespace AKAQ.CodeChallenge.Business
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class NumberConverterService : INumberConverterService
     {
-        string[] NUMBERS = new string[] { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
-
+        string[] NUMBERS = new string[] { "", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN" };
+        string[] TYS = new string[] { "", "TWENTY", "THIRTY", "FOURTY", "FIFTY", "SIXTY", "SEVENTY", "EIGHTY", "NINETY" };
+        string[] TEENS = new string[] { "", "ELEVEN", "TWELVE", "THIRTEEN", "FOURTEEN", "FIFTEEN", "SIXTEEN", "SEVENTEEN", "EIGHTEEN", "NINETEEN" };
+        string[] GROUPS = new string[] {" DOLLARS", " THOUSAND", " MILLION", " BILLION", " TRILLION"};
+        // I stop at 999 Trillion, because those number is huge already. Technically, we can add support for bigger number by extend this GROUPS LIST
+        // Full list of number can be found here - https://www.thoughtco.com/bigger-than-a-trillion-1857463
         public NumberConverterService()
         {
         }
 
         public string ConvertToString(double input)
         {
-            //Round number to 2 decimal place
+            // Round number to 2 decimal place
             double roundedNumber = Math.Round(input, 2);
             double numberWithoutDecimial = Math.Floor(roundedNumber);
-            double numberOfCents = (roundedNumber - numberWithoutDecimial) * 100;
+            // Sometime computer calculate and return very weir number ex: (0.29-0) *100. it give me 28.999999999
+            double numberOfCents = Math.Round((roundedNumber - numberWithoutDecimial) * 100,0);
 
+            string readCents = ReadNumber((int)numberOfCents, " CENTS");
+            // Convert fullnumber to string, then partition it to a chunk of 3 digit and call ReadNumber 
+            // Great, all the logic will be inside ReadNumber function . This approach is greate for unit test because the code only reponsible for read 3 digit number
 
-            return ReadNumber((int)numberOfCents, "Cents");
+            var fullNumberStr = numberWithoutDecimial.ToString();
+            int pos = fullNumberStr.Length;
+            int chunks = 0;
+            string results = "";
+            int chunkValue = 0;
+            while(pos > 0) {
+                if(chunkValue>0) {
+                    results = " AND " + results;
+                }
+
+                //In the case there only 1 digit, we need to read 1 digit to chunk. 
+                string chunk = fullNumberStr.Substring(Math.Max(pos - 3, 0), Math.Min(pos, 3));
+                //this to append group Thousand, Milion, Bilion......
+                chunkValue = int.Parse(chunk);
+                string readChunk = ReadNumber(chunkValue, GROUPS[chunks]);
+                pos -= 3;
+                //If value is 0 for it not a end chunk, append unit it to result . eg. it should read five thousand dollars instead of 5 thousand
+                // Only append DOLLARS, if chunk >1 we will ignore it. eg. 8000000 should read Eight million dollars, if not check it will read eight milions thousand dollars.
+
+                if (chunkValue ==0 && pos>0 && chunks ==0) {
+                    readChunk = GROUPS[chunks];
+                }
+                
+                chunks++;
+                
+                results = readChunk + results;
+
+            }
+            //Only cent, return it without "And"
+            if (string.IsNullOrEmpty(results)) return readCents;
+
+            //if there no cents in the number, dont need to read it
+            return results + (string.IsNullOrEmpty(readCents)?"" : (" AND " + readCents));
         }
         /// <summary>
         /// This function to read the number from 0-999.
@@ -25,19 +68,25 @@ namespace AKAQ.CodeChallenge.Business
         /// <returns>The number.</returns>
         /// <param name="number">Number.</param>
         /// <param name="unit">Unit : cents, thousand, hundress, million, ....</param>
-        public string ReadNumber(int number, string unit) {
-            string result = "";
+        public string ReadNumber(int number, string unit="") {
+            if (number == 0) return string.Empty;
+            //If number from 0 to 10, just return single number read 
+            if (number <= 10) return NUMBERS[number]  + unit;
+            
+            if(number< 20) return TEENS[number-10]  + unit;
+            //the number from 20 to 99 , return read from TYS
+            int ten = number % 10;
+            int ty = number / 10;
 
-            int number1 = number % 10;
-
-            int number2 = (number-number1) / 100;
-            int number3 = (number - number1-number2) % 100;
-
-            if(number1 >0) {
-                result += NUMBERS[number1];
+            if (number < 100) {
+                //if the last digit is 0, dont add a space after first number.
+                return string.Format(ten == 0?"{0}":"{0}-{1}", TYS[ty-1], NUMBERS[ten]) + unit;
             }
+            int hundres = number / 100;
+            //reuse the logic for reading number <100 that we already test and work before, in this case, Unit is empty 
+            string secondPart = ReadNumber(number % 100);
 
-            return result + " " + unit;
+            return NUMBERS[hundres] + " HUNDRED" + (string.IsNullOrEmpty(secondPart)?"":" AND " + secondPart) + unit;
 
         }
     }
--- AKQA.CodeChallenge.Tests/NumberConvertServiceTests.cs
+++ AKQA.CodeChallenge.Tests/NumberConvertServiceTests.cs
@@ -11,13 +11,68 @@ namespace AKQA.CodeChallenge.Tests
         public NumberConvertServiceTests() {
             service = new NumberConverterService();
         }
-
+        // with xUnit we can using CSV or JSON Data, in this example I just use inline for easy read and change it.
         [Theory]
-        [InlineData(0.01, "One Cents")]
+        [InlineData(0.00, "")]
+        [InlineData(0.01, "ONE CENTS")]
+        [InlineData(0.1, "TEN CENTS")]
+        [InlineData(0.8, "EIGHTY CENTS")]
+        [InlineData(0.09, "NINE CENTS")]
+        [InlineData(0.29, "TWENTY-NINE CENTS")]
+        [InlineData(0.101, "TEN CENTS")]
+        [InlineData(0.11, "ELEVEN CENTS")]
+        [InlineData(0.15, "FIFTEEN CENTS")]
+        [InlineData(0.20, "TWENTY CENTS")]
+        [InlineData(0.25, "TWENTY-FIVE CENTS")]
+        [InlineData(0.99, "NINETY-NINE CENTS")]
+        [InlineData(1.00, "ONE DOLLARS")]
+        [InlineData(1.50, "ONE DOLLARS AND FIFTY CENTS")]
+        [InlineData(12.65, "TWELVE DOLLARS AND SIXTY-FIVE CENTS")]
+        [InlineData(20.39, "TWENTY DOLLARS AND THIRTY-NINE CENTS")]
+        [InlineData(100, "ONE HUNDRED DOLLARS")]
+        [InlineData(100.68, "ONE HUNDRED DOLLARS AND SIXTY-EIGHT CENTS")]
+        [InlineData(557.29, "FIVE HUNDRED AND FIFTY-SEVEN DOLLARS AND TWENTY-NINE CENTS")]
+        [InlineData(1000, "ONE THOUSAND DOLLARS")]
+        [InlineData(5000.15, "FIVE THOUSAND DOLLARS AND FIFTEEN CENTS")]
+        [InlineData(9432.46, "NINE THOUSAND AND FOUR HUNDRED AND THIRTY-TWO DOLLARS AND FOURTY-SIX CENTS")]
+        [InlineData(19559.37, "NINETEEN THOUSAND AND FIVE HUNDRED AND FIFTY-NINE DOLLARS AND THIRTY-SEVEN CENTS")]
+        [InlineData(43559.00, "FOURTY-THREE THOUSAND AND FIVE HUNDRED AND FIFTY-NINE DOLLARS")]
+        [InlineData(50000, "FIFTY THOUSAND DOLLARS")]
+        [InlineData(50000.14, "FIFTY THOUSAND DOLLARS AND FOURTEEN CENTS")]
+        [InlineData(800000.2, "EIGHT HUNDRED THOUSAND DOLLARS AND TWENTY CENTS")]
+        [InlineData(7000000, "SEVEN MILLION DOLLARS")]
+        [InlineData(7654321.12, "SEVEN MILLION AND SIX HUNDRED AND FIFTY-FOUR THOUSAND AND THREE HUNDRED AND TWENTY-ONE DOLLARS AND TWELVE CENTS")]
+        [InlineData(13654321.12, "THIRTEEN MILLION AND SIX HUNDRED AND FIFTY-FOUR THOUSAND AND THREE HUNDRED AND TWENTY-ONE DOLLARS AND TWELVE CENTS")]
+        [InlineData(23734321.18, "TWENTY-THREE MILLION AND SEVEN HUNDRED AND THIRTY-FOUR THOUSAND AND THREE HUNDRED AND TWENTY-ONE DOLLARS AND EIGHTEEN CENTS")]
+        [InlineData(10000000, "TEN MILLION DOLLARS")]
+        [InlineData(200000000, "TWO HUNDRED MILLION DOLLARS")]
+        [InlineData(200000012, "TWO HUNDRED MILLION AND TWELVE DOLLARS")]
+        [InlineData(200120012, "TWO HUNDRED MILLION AND ONE HUNDRED AND TWENTY THOUSAND AND TWELVE DOLLARS")]
+        [InlineData(123456789.88, "ONE HUNDRED AND TWENTY-THREE MILLION AND FOUR HUNDRED AND FIFTY-SIX THOUSAND AND SEVEN HUNDRED AND EIGHTY-NINE DOLLARS AND EIGHTY-EIGHT CENTS")]
+        [InlineData(1000000000, "ONE BILLION DOLLARS")]
+        [InlineData(55000000000, "FIFTY-FIVE BILLION DOLLARS")]
+        [InlineData(999999999999.99, "NINE HUNDRED AND NINETY-NINE BILLION AND NINE HUNDRED AND NINETY-NINE MILLION AND NINE HUNDRED AND NINETY-NINE THOUSAND AND NINE HUNDRED AND NINETY-NINE DOLLARS AND NINETY-NINE CENTS")]
+        [InlineData(7000000000000, "SEVEN TRILLION DOLLARS")]
         public void ShouldRead_Cents(double input, string expect)
         {
             var read = this.service.ConvertToString(input);
             Assert.Equal(expect, read);
+        }
+        [Fact]
+        public void ShouldRead_Number_From_1_To_20()
+        {
+            string[] data = new string[] {"ONE","TWO","THREE","FOUR","FIVE",
+                        "SIX","SEVEN","EIGHT","NINE","TEN","ELEVEN","TWELVE",
+                        "THIRTEEN","FOURTEEN","FIFTEEN","SIXTEEN","SEVENTEEN",
+                        "EIGHTEEN","NINETEEN","TWENTY" };
+            for(var i =1; i<= 20; i++) {
+                var read = this.service.ConvertToString((double)i/100);
+                Assert.Equal(data[i-1] +" CENTS", read);
+
+                read = this.service.ConvertToString(i);
+                Assert.Equal(data[i - 1] + " DOLLARS", read);
+            }
+
         }
     }
 }
